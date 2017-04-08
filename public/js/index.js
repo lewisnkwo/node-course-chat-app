b9a34028dae798f5ff5d1598f8933e8c5c7a81dd
Lewis Nkwo <lewisnkwo@Lewiss-MacBook-Pro.local>
2017-04-08 11:05:20 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,18 +3,12 @@ var socket = io();
 //ON
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.on('newMessage', function (message) {
-    console.log('New message:', message);
-  });
-
-  socket.emit('createMessage', {
-    to: 'server@example',
-    text: 'Creating new message'
-  });
-
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage:', message);
+})
