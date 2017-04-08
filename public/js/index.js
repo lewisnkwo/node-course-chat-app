11d55bd8b3dae89f307e4e68e75dcfb4e3c1aada
Lewis Nkwo <lewisnkwo@Lewiss-MacBook-Pro.local>
2017-04-08 11:37:35 +0100

    Greet new user, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,15 @@ var socket = io();
 //ON
 socket.on('connect', function () {
   console.log('Connected to server');
+
+  socket.on('adminMessage', function (adminMessage) {
+    console.log(adminMessage);
+  });
+
+  socket.on('newUserMessage', function (newUserMessage) {
+    console.log(newUserMessage);
+  });
+
 });
 
 socket.on('disconnect', function () {
